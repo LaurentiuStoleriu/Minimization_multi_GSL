4a0d2bb6a7c612dde88c266d283d79daee590871
ayjez <ayjez@ROG>
2020-03-29 20:23:37 +0300

    fara functiile pentru minimizare 1D

--- Minimization_multi_GSL.cpp
+++ Minimization_multi_GSL.cpp
@@ -12,9 +12,6 @@ double p[NMAX], r[NMAX];	// sirurile de pozitii si de raze
 
 double v(int k, double loco_p[]);
 double fn1(const gsl_vector q[], void* params);
-//double my_f(const gsl_vector* v, void* params);
-//void my_df(const gsl_vector* v, void* params, gsl_vector* df);
-//void my_fdf(const gsl_vector* x, void* params, double* f, gsl_vector* df);
 
 int main(void)
 {
@@ -147,33 +144,3 @@ double fn1(const gsl_vector q[], void* params)
 	return f;
 }
 
-// nefolosit: doar pentru doua
-
-// double my_f(const gsl_vector* v, void* params)
-// {
-// 	double x, y;
-// 	double* p = (double*)params;
-// 
-// 	x = gsl_vector_get(v, 0);
-// 	y = gsl_vector_get(v, 1);
-// 
-// 	return p[2] * (x - p[0]) * (x - p[0]) + p[3] * (y - p[1]) * (y - p[1]) + p[4];
-// }
-// 
-// void my_df(const gsl_vector* v, void* params, gsl_vector* df)
-// {
-// 	double x, y;
-// 	double* p = (double*)params;
-// 
-// 	x = gsl_vector_get(v, 0);
-// 	y = gsl_vector_get(v, 1);
-// 
-// 	gsl_vector_set(df, 0, 2.0 * p[2] * (x - p[0]));
-// 	gsl_vector_set(df, 1, 2.0 * p[3] * (y - p[1]));
-// }
-// 
-// void my_fdf(const gsl_vector* x, void* params, double* f, gsl_vector* df)
-// {
-// 	*f = my_f(x, params);
-// 	my_df(x, params, df);
-// }
